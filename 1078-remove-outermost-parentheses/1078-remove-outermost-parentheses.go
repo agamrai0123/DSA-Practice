b095f05d10903691f1fd545c7022f1a59b2eaa63
agamrai0123 <agam.rai0123@gmail.com>
2025-05-07 00:08:55 +0530

    Time: 10 ms (27.17%), Space: 9.3 MB (25%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.go
@@ -0,0 +1,18 @@
+func removeOuterParentheses(s string) string {
+    res := ""
+    ind := 0
+    for _, ch := range s{
+        if ch == '('{
+            if ind > 0{
+                res = res + string(ch)
+            } 
+            ind++
+        } else if ch == ')'{
+            ind--
+            if ind > 0{
+                res = res + string(ch)
+            }
+        }
+    }
+    return res
+}
