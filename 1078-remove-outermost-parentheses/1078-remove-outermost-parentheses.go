1e606e81ba986372fa41471113a10e50a9424376
agamrai0123 <agam.rai0123@gmail.com>
2025-07-26 11:34:23 +0530

    Time: 14 ms (8.11%), Space: 14.1 MB (5.41%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.go
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.go
@@ -1,11 +1,11 @@
 func removeOuterParentheses(s string) string {
     res := ""
     ind := 0
-    for _, ch := range s{
+    for _, ch := range s {
         if ch == '('{
             if ind > 0{
                 res = res + string(ch)
-            } 
+            }
             ind++
         } else if ch == ')'{
             ind--
